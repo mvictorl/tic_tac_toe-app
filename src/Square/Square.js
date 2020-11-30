99829e8970f9c8cf88851cf3d9fbbf63042a0342
MVictorL <mvictorl@gmail.com>
2020-11-30 11:22:52 +0300

    Add Square constructor

--- src/Square/Square.js
+++ src/Square/Square.js
@@ -2,6 +2,14 @@ import React, { Component } from "react"
 
 class Square extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      value:null
+    }
+  }
+  
+
   render() {
     return (
       <button 
