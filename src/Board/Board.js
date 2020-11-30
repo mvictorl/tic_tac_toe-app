4888dc02991604c2099faaf47452b7c6d1b0bedd
MVictorL <mvictorl@gmail.com>
2020-11-30 13:10:47 +0300

    Add function calculateWinner(squares) & change status handler

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -32,8 +32,34 @@ class Board extends Component {
     )
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   render() {
-    const status = 'Следующий ход игрока - ' + (this.state.xIsNext ? 'X' : 'O')
+    const winner = this.calculateWinner(this.state.squares)
+    let status
+    if(winner) {
+      status = 'Выиграл игрок - ' + winner
+    } else {
+      status = 'Следующий ход игрока - ' + (this.state.xIsNext ? 'X' : 'O')
+    }
 
     return (
       <div>
